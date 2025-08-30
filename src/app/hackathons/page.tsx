13b9414200f4e678f00467ddd6dedea87c74cdce
Chris Yoo <c.yoo@student.unsw.edu.au>
2025-08-30 11:45:59 -0400

    sf update

--- src/app/hackathons/page.tsx
+++ src/app/hackathons/page.tsx
@@ -2,13 +2,17 @@ import HackathonCard from "@/components/HackathonCard";
 import hackathons from "@/data/hackathons.json";
 
 export default function Hackathons() {
+  const hackathonsTotal = hackathons.length;
+  const hackathonsWon = hackathons.filter(
+    (hackathon) => hackathon.award
+  ).length;
+
   return (
     <main className="container mx-auto px-4 py-12">
       <section className="text-center">
         <h2 className="text-4xl font-bold mb-4">Hackathons</h2>
-        <p className="text-xl mb-8">
-          I&apos;ve participated in several hackathons over the years. Here is a
-          compilation of them.
+        <p className="italic">
+          {hackathonsWon} wins, {hackathonsTotal} total
         </p>
         <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {hackathons.map((hackathon, index) => (
